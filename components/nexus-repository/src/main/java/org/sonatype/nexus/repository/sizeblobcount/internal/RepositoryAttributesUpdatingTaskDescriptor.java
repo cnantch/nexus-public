f485a72260a62b1bcd4611f665e8c571c64abef2
Cyrille NANTCH <cyrille.nantchouang@gmail.com>
2017-12-18 13:36:22 +0100

    Render the task for calclating the size and the blob count pof a repository

--- components/nexus-repository/src/main/java/org/sonatype/nexus/repository/sizeblobcount/internal/RepositoryAttributesUpdatingTaskDescriptor.java
+++ components/nexus-repository/src/main/java/org/sonatype/nexus/repository/sizeblobcount/internal/RepositoryAttributesUpdatingTaskDescriptor.java
@@ -1,10 +1,15 @@
 package org.sonatype.nexus.repository.sizeblobcount.internal;
 
+import org.sonatype.nexus.formfields.FormField;
+import org.sonatype.nexus.formfields.RepositoryCombobox;
+import org.sonatype.nexus.repository.sizeblobcount.RepositoryAttributesFacet;
 import org.sonatype.nexus.scheduling.TaskDescriptorSupport;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
+
+import static org.sonatype.nexus.repository.RepositoryTaskSupport.REPOSITORY_NAME_FIELD_ID;
 
 /**
  * @since 3.7.0
@@ -17,6 +22,18 @@ public class RepositoryAttributesUpdatingTaskDescriptor extends TaskDescriptorSu
 
     @Inject
     public RepositoryAttributesUpdatingTaskDescriptor() {
-        super(TYPE_ID, RepositoryAttributesUpdatingTask.class, "Update size and the blob count", NOT_VISIBLE, NOT_EXPOSED);
+        super(TYPE_ID,
+                RepositoryAttributesUpdatingTask.class,
+                "Update size and the blob count",
+                VISIBLE,
+                EXPOSED,
+                new RepositoryCombobox(
+                        REPOSITORY_NAME_FIELD_ID,
+                        "Repository",
+                        "Select the repository to purge unused snapshot versions from",
+                        FormField.MANDATORY
+                ).includingAnyOfFacets(RepositoryAttributesFacet.class).includeAnEntryForAllRepositories()
+        );
+       // super(TYPE_ID, RepositoryAttributesUpdatingTask.class, "Update size and the blob count", NOT_VISIBLE, NOT_EXPOSED);
     }
 }
